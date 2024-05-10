d2b9385be6cbb9b25d3ebad55bd2dd72db8483f6
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-05-10 22:54:36 +0100

    feat: add coin market price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image";
+
+import { IlAds } from "@/assets";
 import HomeHeroSection from "@/components/HomeHeroSection";
 import HomeServiceSection from "@/components/HomeServiceSection";
 import HomeContactSection from "@/components/HomeContactSection";
@@ -6,9 +10,28 @@ import HomeContactSection from "@/components/HomeContactSection";
 export default function Home() {
   return (
     <main className="flex-1 flex flex-col space-y-32">
-      <div className="flex flex-col space-y-8">
-        <HomeHeroSection />
-        <HomeServiceSection />
+      <div className="flex flex-col space-y-16">
+        <div className="space-y-4">
+          <iframe
+            src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover="
+            width="100%"
+            height="36px"
+            scrolling="auto"
+            marginWidth={0}
+            marginHeight={0}
+            frameBorder={0}
+          />
+          <div className="px-4">
+            <Image
+              src={IlAds}
+              alt="ads"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col space-y-8">
+          <HomeHeroSection />
+          <HomeServiceSection />
+        </div>
       </div>
       <HomeContactSection />
     </main>
--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,1 +1,2 @@
 export * as IlSpiral from "./il_spiral.png";
+export * as IlAds from "./il_ads.svg";
